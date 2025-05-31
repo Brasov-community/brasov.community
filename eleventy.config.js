cd6d90b44a974053705926941814d8ff3b6ff44a
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2025-05-31 11:27:07 +0300

    Refactor: Fix code style
    Simply saved the files in VS Code.

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,7 +15,7 @@ dotenv.config();
 import yaml from 'js-yaml';
 
 //  config import
-import {getAllPosts, showInSitemap, tagList} from './src/_config/collections.js';
+import { getAllPosts, showInSitemap, tagList } from './src/_config/collections.js';
 import events from './src/_config/events.js';
 import filters from './src/_config/filters.js';
 import plugins from './src/_config/plugins.js';
