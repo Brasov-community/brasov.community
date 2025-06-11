7f8908898bf0ea8e302fc4df0b29c8315073c1c3
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2025-06-11 23:37:20 +0300

    Add "Sedintele Consiliului Local" page

--- src/_config/collections.js
+++ src/_config/collections.js
@@ -3,6 +3,11 @@ export const getAllPosts = collection => {
   return collection.getFilteredByGlob('./src/posts/**/*.md').reverse();
 };
 
+/** All wiki pages as a collection. */
+export const getAllWikis = collection => {
+  return collection.getFilteredByGlob('./src/pages/wikis/**/*.md');
+};
+
 /** All relevant pages as a collection for sitemap.xml */
 export const showInSitemap = collection => {
   return collection.getFilteredByGlob('./src/**/*.{md,njk}');
